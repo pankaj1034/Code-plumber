4e577b51bdf585fc1925df2d445ba8115777d654
pankaj1034 <IEC2021094@iiita.ac.in>
2024-09-08 18:11:28 +0530

    Add files via upload

---
+++ server/runEnv/code/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+  
+int main() {
+    int t;
+    cin >> t; // Number of test cases
+    while(t--){
+      cout<<"Hello World"<<endl;
+    }
+    return 0;
+}
